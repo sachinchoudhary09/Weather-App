c76e279e2f24364f41431b1e533e054ebcd3c5a6
Sachin Choudhary <sachinchoudhary5711@gmail.com>
2023-09-18 18:10:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// - - - - - - - - - - - -Tab Handling- - - - - - - - - - - -
+// Tab Handling
 const userTab = document.querySelector("[data-userWeather]");
 const searchTab = document.querySelector("[data-searchWeather]");
 const userContainer = document.querySelector(".weather-container");
